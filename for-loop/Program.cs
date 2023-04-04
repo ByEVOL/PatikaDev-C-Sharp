2db9b9d66d1617b278b1bbb5c40005f7ef91d253
Emre İLHAN <42745513+ByEVOL@users.noreply.github.com>
2023-04-04 05:49:19 +0300

    Add files via upload

---
+++ for-loop/Program.cs
@@ -0,0 +1,39 @@
+﻿
+// Ekrandan girilen sayıya kadar olan tek sayılarını ekrana yazdır.
+Console.Write("Lütfen Bir Sayı Giriniz: ");
+int sayac = int.Parse(Console.ReadLine());
+for (int i = 1; i <= sayac; i++)
+{
+    if (i % 2 == 1)
+        Console.WriteLine(i);
+}
+
+// 1 ile 1000 arasında tek ve çift sayıların kendi içlerindeki toplamını ekrana yazdırır
+int tekToplam = 0;
+int ciftToplam = 0;
+for (int i = 1; i <= 1000; i++)
+{
+    if (i % 2 == 1)
+        tekToplam += i; // tekToplam = tekToplam + i;
+    else
+        ciftToplam += i; // tekToplam = tekToplam + i;
+}
+
+Console.WriteLine("Tek Toplam: " + tekToplam);
+Console.WriteLine("Çift Toplam: " + ciftToplam);
+
+
+for (int i = 1; i < 10; i++)
+{
+    if (i == 4)
+        break;
+    Console.WriteLine(i);
+}
+
+for (int i = 1; 1 < 10; i++)
+{
+    if (i == 4)
+        continue;
+    Console.WriteLine(i);
+}
+
---
+++ switch-case/Program.cs
@@ -0,0 +1,40 @@
+﻿int month = DateTime.Now.Month;
+
+switch (month)
+{
+    // tek tek case belirleme
+    case 12:
+        Console.WriteLine("Aralık Ayındasınız!");
+        break;
+    case 1:
+        Console.WriteLine("Ocak Ayındasınız!");
+        break;
+    case 3:
+        Console.WriteLine("Mart Ayındasınız!");
+        break;
+    case 2:
+        Console.WriteLine("Şubat Ayındasınız!");
+        break;
+
+    default:
+        Console.WriteLine("Yanlış Veri Girişi!");
+        break;
+}
+
+switch (month)
+{
+    case 12:
+    case 1:
+    case 2:
+        Console.WriteLine("Kış Ayındasınız");
+        break;
+
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine("İlk Bahardasınız!");
+        break;
+
+    default:
+        break;
+}
