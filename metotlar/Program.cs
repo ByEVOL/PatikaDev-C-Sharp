37937018d3b5981bc45422f78e59d422e8a18849
Emre İLHAN <42745513+ByEVOL@users.noreply.github.com>
2023-04-09 00:26:40 +0300

    Add files via upload

---
+++ metotlar/Program.cs
@@ -0,0 +1,43 @@
+﻿
+// erisim_belirteci geri_donustipi metot_adi(parametreListesi/arguman)
+// {
+// komutlar; 
+// return;
+// }
+
+int a = 2;
+int b = 3;
+Console.WriteLine(a+b);
+
+//döndürülen
+int sonuc = Topla(a,b);
+Console.WriteLine(sonuc);
+
+Metotlar ornek = new Metotlar();
+ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+int sonuc2 = ornek.ArttırVeTopla(ref a, ref b);
+ornek.EkranaYazdir(Convert.ToString(sonuc2));
+ornek.EkranaYazdir(Convert.ToString(a+b));
+
+
+static int Topla(int deger1, int deger2)
+{
+    return (deger1 + deger2);
+}
+
+
+class Metotlar
+{
+    public void EkranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+
+    public int ArttırVeTopla(ref int deger1, ref int deger2)
+    {
+        deger1+=1;
+        deger2+=1;
+        return deger1+deger2;
+    }
+}
